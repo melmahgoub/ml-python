98dc5163c3c71650e6aef6dc9a5e57681045e19a
mawada <74012786+melmahgoub@users.noreply.github.com>
2021-04-29 12:48:40 +0100

    Update DSC201-401 Final Project - Part 1.py

--- DSC201-401 Final Project - Part 1.py
+++ DSC201-401 Final Project - Part 1.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Question 1
-
-# ### A 
-
-# I used the command cp /public/bmort/R/wheat-unknown.csv . and cp /public/bmort/R/wheat.csv .
-
-# ### B
-
-# In[1]:
-
-
 data1<- read.csv("wheat.csv")
 
+colSums(is.na(data1)) # There are no missing Values
 
-# In[2]:
+summary(data1) # The ranges of the values differ mostly.
 
-
-colSums(is.na(data1))
-
-
-# There are no missing Values
-
-# ### C
-
-# In[3]:
-
-
-summary(data1)
-
-
-# The ranges of the values differ mostly. Groove, width and length have a similar range. Area and perimeter also have a similar range of values. All of the values have similar magnitude, as in each column the values are within the same powers of 10.
-
-# ### D
-
-# In[4]:
-
+'''
+Groove, width and length have a similar range. Area and perimeter also have a similar range of values. 
+All of the values have similar magnitude, as in each column the values are within the same powers of 10.
+'''
 
 library(class)
 library(caret)
-
-
-# In[5]:
-
 
 n<- 100
 sample_rate<- 0.8
@@ -61,9 +31,6 @@ y_test<- data1[testingrows,]$type
 y_test
 
 
-# In[6]:
-
-
 train_rows<- createDataPartition(y=data1$type, p=0.8, list=F)
 training<- data1[train_rows,]
 head(training)
@@ -71,47 +38,24 @@ testing<- data1[-train_rows,]
 head(testing)
 
 
-# ### E
-
-# In[7]:
-
-
 library(caret)
-
-
-# In[8]:
-
 
 trctrl<- trainControl(method="repeatedcv", number=10, repeats=3)
 svm_linear<- train(type~., data=training, method="svmLinear", trControl=trctrl,PreProcess=c("center", "scale"))
 
-
-# In[9]:
-
-
 svm_linear
-
-
-# ### F
-
-# In[10]:
 
 
 test_pred<- predict(svm_linear, newdata=testing)
 test_pred
 
 
-# In[11]:
-
-
 confusionMatrix(test_pred, testing$type)
 
 
-# The accuracy of this test is 94.74%, which is generally good. However, since the confidence interval is so wide, (82.25%, 99.36%), this result is not that reliable.
-
-# ## G
-
-# In[12]:
+'''
+The accuracy of this test is 94.74%, which is generally good. However, since the confidence interval is so wide, (82.25%, 99.36%), this result is not that reliable.
+'''
 
 
 testdata<- read.csv("wheat-unknown.csv")
@@ -119,20 +63,7 @@ testfit<- predict(svm_linear, newdata=testdata)
 testfit
 
 
-# In[13]:
-
 
 testdata$type<- testfit
 
-
-# In[14]:
-
-
 testdata
-
-
-# In[ ]:
-
-
-
-
